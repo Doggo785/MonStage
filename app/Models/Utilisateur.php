3083c19404178fb3cd9b821531af7515ee7d2150
Doggo785 <stephane.plathey@proton.me>
2025-03-26 13:45:51 +0000

    Add Eloquent models for Avis, Candidature, Competence, Entreprise, Etudiant, Offre, Offres_Competences, Region, Role, Secteur, Statuts_Candidature, Utilisateur, Ville, and Wishlist with relationships and properties

--- app/Models/Utilisateur.php
+++ app/Models/Utilisateur.php
@@ -4,7 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Utilisateur extends Model
-{
-    //
+class Utilisateur extends Model {
+    protected $table = 'Utilisateur';
+    protected $primaryKey = 'ID_User';
+    public $timestamps = false;
+
+    // Relation avec Etudiant
+    public function etudiant() {
+        return $this->hasOne(Etudiant::class, 'ID_User');
+    }
+
+    // Relation avec Role
+    public function role() {
+        return $this->belongsTo(Role::class, 'ID_Role');
+    }
 }
