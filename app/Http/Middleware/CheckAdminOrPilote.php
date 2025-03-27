827e8a6b4c61e6ba54bc7b2ea8c251f4449c5e4a
Doggo785 <stephane.plathey@proton.me>
2025-03-27 17:22:59 +0000

    Add middleware to check if user is Admin or Pilote

---
+++ app/Http/Middleware/CheckAdminOrPilote.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class CheckAdminOrPilote
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        // Vérifie si l'utilisateur est connecté
+        if (auth()->check()) {
+            // Récupère l'utilisateur connecté
+            $utilisateur = auth()->user();
+
+            // Vérifie si le rôle de l'utilisateur est "Pilote"
+            if ($utilisateur->role->Libelle === 'Pilote' || $utilisateur->role->Libelle === 'Administrateur') {
+                return $next($request);
+            }
+        }
+
+        // Redirige ou retourne une réponse d'erreur si l'utilisateur n'est pas Pilote
+        return response()->json(['error' => 'Accès non autorisé'], 403);
+    }
+}
